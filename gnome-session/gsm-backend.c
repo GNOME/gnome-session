33dbee72d97f7ac5e3315f437799dc3d1fdc1e44
Elliot Lee <sopwith@src.gnome.org>
1997-12-17 16:50:47 +0000

    mo fixes - sopwith

--- gnome-session/gsm-backend.c
+++ gnome-session/gsm-backend.c
@@ -1,18 +1,16 @@
 #include "gsm.h"
-#include <SM/SMlib.h>
 
 gchar *smsavedir = NULL;
 gchar *current_session = NULL;
 
 void gsm_session_init(void)
 {
-  
+  g_warning("not implemented yet\n");
 }
 
 void gsm_session_run(gchar *session_name)
 {
   g_error("Not implemented yet\n");
-  return;
 }
 
 void gsm_session_end_current(void)
