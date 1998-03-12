7bd2c67a232f4c82c575708cdec7536c7f86bb76
Tom Tromey <tromey@src.gnome.org>
1998-03-12 06:10:51 +0000

    Corrected copyright error.  - tromey@cygnus.com

--- gnome-session/save.c
+++ gnome-session/save.c
@@ -1,6 +1,6 @@
 /* save.c - Code to save session.
 
-   Copyright (C) 1998 Free Software Foundation, Inc.
+   Copyright (C) 1998 Tom Tromey
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
