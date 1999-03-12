56d32ebcb307a03767469b45f28612e0570d83c3
Felix Bellaby <felix@src.gnome.org>
1999-03-12 23:34:54 +0000

    remove dependency on iceauth and memory leak on client death

--- gnome-session/ice.c
+++ gnome-session/ice.c
@@ -26,16 +26,34 @@
 #include <gdk/gdk.h>
 #include <gtk/gtk.h>
 
-#include "libgnomeui/gnome-ice.h"
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <libgnomeui/gnome-ice.h>
+#include <libgnome/libgnome.h>
 
 #include "manager.h"
-#include "auth.h"
+#include "session.h"
 
-/* Number of transports.  */
-static int num_transports;
+#define MAGIC_COOKIE_LEN 16
+#define ENVNAME "SESSION_MANAGER"
 
-/* Authentication records (don't ask me).  */
-static IceAuthDataEntry *auth_entries;
+/* Network ids.  */
+char* ids;
+
+/* The "sockets" which we listen on */
+static IceListenObj *sockets;
+static gint *input_id;
+static guint num_sockets;
+
+/* File containing the ICE authorization data (usually ~/.ICEauthority) */
+static gchar* authfile;
+
+static IceAuthFileEntry* file_entry_new (const gchar* protocol, 
+					 const IceListenObj socket);
+static GSList* read_authfile(gchar* filename);
+static void write_authfile(gchar* filename, GSList* entries);
 
 /* This is called when a client tries to connect.  We accept most
    connections.  We reject connections while doing a shutdown.  */
@@ -62,55 +80,245 @@ accept_connection (gpointer client_data, gint source,
       gtk_main_iteration ();
       status2 = IceConnectionStatus (connection);
     }
-#if 0
-  /* I think that the changes to gnome-ice.c mean that the connection
-     is being closed before we get here - FIXME: check this */
-  if (status2 != IceConnectAccepted)
-    IceCloseConnection (connection);
-#endif
 }
 
-/* FIXME: error message text could be returned somehow.  Or at least
-   printed.  */
-int
+typedef struct {
+  IceConn        connection;
+  GDestroyNotify clean_up;
+  gpointer       data;
+} Watch;
+
+static GSList* watch_list = NULL;
+
+static void
+ice_watch (IceConn connection, IcePointer client_data, Bool opening,
+	   IcePointer *watch_data)
+{
+  if (opening)
+    {
+      Watch *watch = g_new0 (Watch, 1); 
+      watch->connection = connection;
+      *watch_data = (IcePointer)watch;
+      APPEND (watch_list, watch);
+    }
+  else 
+    {
+      Watch *watch = (Watch*)*watch_data;
+      if (watch->clean_up)
+	watch->clean_up (watch->data);
+      g_free (watch);
+    }
+}
+
+void 
+ice_set_clean_up_handler (IceConn connection, 
+			  GDestroyNotify clean_up, gpointer data)
+{
+  GSList* list;
+  for (list = watch_list; list; list = list->next)
+    {
+      Watch *watch = (Watch*)list->data;
+      if (watch->connection == connection)
+	{
+	  watch->clean_up = clean_up;
+	  watch->data     = data;
+	  REMOVE (watch_list, watch);	  
+	  break;
+	}
+    }
+}
+
+/*
+ * Host Based Authentication Callback.
+ * Refuse all connections that fail MIT-MAGIC-COOKIE-1 authentication.
+ */
+static Bool
+auth_proc (char* hostname)
+{
+    return 0; /* always reject */
+}
+
+void
 initialize_ice (void)
 {
   char error[256];
   IceListenObj *listeners;
-  int i;
-  char *ids, *p;
+  char *p;
+  guint i;
+  GSList* entries;
 
   gnome_ice_init ();
 
-  /* We don't care about the previous IO error handler.  */
-  IceSetIOErrorHandler (io_error_handler);
+  IceAddConnectionWatch (ice_watch, NULL);
 
-  if (! SmsInitialize ("GnomeSM", VERSION, new_client, NULL,
-		       HostBasedAuthProc, sizeof error, error))
-    return 0;
-
-  if (! IceListenForConnections (&num_transports, &listeners,
+  if (! SmsInitialize (GsmVendor, VERSION, new_client, NULL,
+		       auth_proc, sizeof error, error) ||
+      ! IceListenForConnections (&num_sockets, &sockets,
 				 sizeof error, error))
-    return 0;
+    {
+      g_warning (error);
+      exit (1);
+    }
 
-  if (! SetAuthentication (num_transports, listeners, &auth_entries))
-    return 0;
+  input_id = g_new (gint, num_sockets);
 
-  for (i = 0; i < num_transports; ++i)
-    gdk_input_add (IceGetListenConnectionNumber (listeners[i]),
-		   GDK_INPUT_READ, accept_connection,
-		   (gpointer) listeners[i]);
+  authfile = IceAuthFileName ();
 
-  ids = IceComposeNetworkIdList (num_transports, listeners);
-#define ENVNAME "SESSION_MANAGER"
+  entries = read_authfile (authfile);
+
+  for (i = 0; i < num_sockets; i++)
+    {
+      APPEND (entries, file_entry_new ("ICE", sockets[i]));
+      APPEND (entries, file_entry_new ("XSMP", sockets[i]));
+      
+      input_id[i] = gdk_input_add (IceGetListenConnectionNumber (sockets[i]),
+				   GDK_INPUT_READ, accept_connection,
+				   (gpointer) sockets[i]);
+
+      IceSetHostBasedAuthProc (sockets[i], auth_proc);
+    }
+
+  write_authfile (authfile, entries);
+
+  ids = IceComposeNetworkIdList (num_sockets, sockets);
+
   p = g_strconcat (ENVNAME "=", ids, NULL);
   putenv (p);
-
-  return 1;
+  g_free (p);
 }
 
 void
 clean_ice (void)
 {
-  FreeAuthenticationData (num_transports, auth_entries);
+  guint i;
+  GSList* entries;
+
+  entries = read_authfile (authfile);
+
+  for (i = 0; i < num_sockets; i++)
+    {
+      char* network_id = IceGetListenConnectionString (sockets[i]);
+      GSList* list = entries;
+      
+      while (list)
+	{
+	  IceAuthFileEntry *file_entry = (IceAuthFileEntry *)list->data;
+
+	  list = list->next;
+	  if (!strcmp (file_entry->network_id, network_id))
+	    {
+	      REMOVE (entries, file_entry);
+	      IceFreeAuthFileEntry (file_entry);
+	    }
+	}
+
+      gtk_input_remove (input_id[i]);      
+      free (network_id);
+    }
+
+  write_authfile (authfile, entries);
+
+  g_free (input_id);
+  free (ids);
+  IceFreeListenObjs (num_sockets, sockets);
+}
+
+/* Creates a new entry for inclusion in the ICE authority file and
+ * tells ICElib that we are using it to vailidate connections. */
+static IceAuthFileEntry *
+file_entry_new (const gchar* protocol, const IceListenObj socket)
+{
+  IceAuthFileEntry *file_entry;
+  IceAuthDataEntry data_entry;
+  
+  file_entry = (IceAuthFileEntry *) malloc (sizeof (IceAuthFileEntry));
+
+  file_entry->protocol_name = strdup (protocol);
+  file_entry->protocol_data = NULL;
+  file_entry->protocol_data_length = 0;
+  file_entry->network_id = IceGetListenConnectionString (socket);
+  file_entry->auth_name = strdup ("MIT-MAGIC-COOKIE-1");
+  file_entry->auth_data = IceGenerateMagicCookie (MAGIC_COOKIE_LEN);
+  file_entry->auth_data_length = MAGIC_COOKIE_LEN;
+
+  data_entry.protocol_name = file_entry->protocol_name;
+  data_entry.network_id = file_entry->network_id;
+  data_entry.auth_name = file_entry->auth_name;
+  data_entry.auth_data = file_entry->auth_data;
+  data_entry.auth_data_length = file_entry->auth_data_length;
+
+  IceSetPaAuthData (1, &data_entry);
+
+  return file_entry;
+}
+
+static gint original_umask;
+
+/* Reads a file containing ICE authority data */
+static GSList* 
+read_authfile(gchar* filename)
+{
+  GSList* entries = NULL;
+  FILE *fp;
+
+  original_umask = umask (0077);
+
+  if (IceLockAuthFile (filename, 10, 2, 600) != IceAuthLockSuccess)
+    {
+      g_warning ("Unable to lock ICE authority file: %s", filename);
+      IceFreeListenObjs (num_sockets, sockets);
+      exit (1);
+    }
+
+  fp = fopen (filename, "rb");
+
+  if (fp) {
+    IceAuthFileEntry *file_entry;
+    
+    while ((file_entry = IceReadAuthFileEntry (fp)) != NULL)
+      APPEND (entries, file_entry);
+
+    fclose (fp);
+  } else {
+    struct stat st;
+
+    if (stat (filename, &st) == 0)
+      {
+	g_warning ("Unable to read ICE authority file: %s", filename);
+	IceUnlockAuthFile (filename);
+	IceFreeListenObjs (num_sockets, sockets);
+	exit (1);
+      }
+  }
+  return entries;
+}
+
+/* Writes a file of ICE authority data */
+static void 
+write_authfile (gchar* filename, GSList* entries)
+{
+  GSList *list = entries; 
+  FILE *fp;
+
+  fp = fopen (filename, "wb");
+  if (fp) {
+    while (list)
+      {
+	IceAuthFileEntry *file_entry = (IceAuthFileEntry *)list->data;
+
+	REMOVE (list, file_entry);
+
+	IceWriteAuthFileEntry (fp, file_entry);	
+	IceFreeAuthFileEntry (file_entry);
+      }
+    fclose (fp);
+  } else {
+    g_warning ("Unable to write to ICE authority file: %s", filename);
+    IceUnlockAuthFile (filename);
+    IceFreeListenObjs (num_sockets, sockets);
+    exit (1);
+  }
+  IceUnlockAuthFile (filename);
+
+  umask (original_umask);
 }
