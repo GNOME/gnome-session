ee0c3de0a34bb7a399ee8e2708b150e195f489a0
jacob berkman <jacob@ximian.com>
2001-02-15 23:27:12 +0000

    still look in user dir if session is `Default'
    2001-02-15  jacob berkman  <jacob@ximian.com>
    
    	* save.c (read_session): still look in user dir if session is
    	`Default'

--- gnome-session/save.c
+++ gnome-session/save.c
@@ -399,13 +399,9 @@ read_session (const char *name)
   session->handle = command_handle_new ((gpointer)session);
  
   if(name) {
-    if(!g_strcasecmp(name,DEFAULT_SESSION))
+    list = read_clients (CONFIG_PREFIX,name,MATCH_ID);
+    if (!list)
       list = read_clients (DEFAULT_CONFIG_PREFIX,name,MATCH_FAKE_ID);
-    else {
-      list = read_clients (CONFIG_PREFIX,name,MATCH_ID);
-      if (!list)
-	list = read_clients (DEFAULT_CONFIG_PREFIX,name,MATCH_FAKE_ID);
-    }
   } 
   
   /* If the the specified session from the command line doesn't
