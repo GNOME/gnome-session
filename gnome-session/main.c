d8244f4b99e1b01d66cb97a065e280721fc439ca
Glynn Foster <glynn.foster@sun.com>
2001-11-03 11:55:46 +0000

    Align with the gnome_client_get_destop_id in libgnomeui for the global
    2001-11-03  Glynn Foster <glynn.foster@sun.com>
    
    	* main.c: Align with the gnome_client_get_destop_id in libgnomeui
    	for the global session ID. GNOME_DESKTOP_SESSION_ID.

--- gnome-session/main.c
+++ gnome-session/main.c
@@ -183,7 +183,7 @@ main (int argc, char *argv[])
   if(failsafe)
 	session_name = FAILSAFE_SESSION;
   
-  session_name_env = g_strconcat ("GNOME_SESSION_NAME=", g_strdup (session_name), NULL);
+  session_name_env = g_strconcat ("GNOME_DESKTOP_SESSION_ID=", g_strdup (session_name), NULL);
   putenv (session_name_env);
   the_session = read_session (session_name);
 
