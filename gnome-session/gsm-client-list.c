c99fe65d554dec0020de11014fcf4a9f438265d0
Kjartan Maraas <kmaraas@gnome.org>
2002-01-31 00:33:42 +0000

    Merge fix for crash on double delete from Glynn from stable.
    2002-01-31  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* gsm-client-list.c: Merge fix for crash on double delete
    	from Glynn from stable.

--- gnome-session/gsm-client-list.c
+++ gnome-session/gsm-client-list.c
@@ -413,20 +413,23 @@ void
 gsm_client_list_remove_selection (GsmClientList* client_list)
 {
   GtkCList* clist = (GtkCList*)client_list;
+  gint row = GPOINTER_TO_INT (clist->selection->data);
 
   g_return_if_fail(client_list != NULL);
   g_return_if_fail(GSM_IS_CLIENT_LIST(client_list));
 
   gtk_clist_freeze (clist);
   {
-    gint row = GPOINTER_TO_INT (clist->selection->data);
     gpointer data = gtk_clist_get_row_data (clist, row);
     GsmClientRow* client_row = GSM_CLIENT_ROW (data);
     
-    gsm_client_row_remove (client_row);
-    register_change (client_list, client_row, GSM_CLIENT_ROW_REMOVE);
+    if (client_row) {
+      gsm_client_row_remove (client_row);
+      register_change (client_list, client_row, GSM_CLIENT_ROW_REMOVE);
+    }
   }
   gtk_clist_thaw (clist);
+  gtk_clist_select_row (clist, row - 1, 0);
 }
 
 static void
