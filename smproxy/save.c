dece61e6b3c69d41148389798b1c37260a21ff92
Kjartan Maraas <kmaraas@src.gnome.org>
2005-05-16 22:49:52 +0000

    Revert accidental commit

--- smproxy/save.c
+++ smproxy/save.c
@@ -396,13 +396,10 @@ WriteProxyFile ()
     const char *path;
     WinInfo *winptr;
     Bool success = False;
-    Bool path_should_be_freed = False;
 
     path = getenv ("SM_SAVE_DIR");
-    if (!path) {
+    if (!path)
       path = gnome_util_home_file (NULL);
-      path_should_be_freed = True;
-    }
     if (!path)
       path = g_get_home_dir ();
     if (!path)
@@ -438,9 +435,6 @@ WriteProxyFile ()
     }
 
  bad:
-
-    if (path_should_be_freed == True)
-	g_free (path);
 
     if (proxyFile)
 	fclose (proxyFile);
