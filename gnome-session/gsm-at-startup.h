2655e5730d64257cca1606dc7f9c509539163ebd
Vincent Untz <vuntz@gnome.org>
2007-02-05 22:41:53 +0000

    Revert patch committed on 2007-01-06 since the gnome-control-center
    2007-02-05  Vincent Untz  <vuntz@gnome.org>
    
    	* gsm-at-startup.[ch]: (gsm_assistive_tech_exec)
    	(gsm_assistive_registry_start)
    	(gsm_assistive_technologies_start)
    	* main.c: (main)
    	Revert patch committed on 2007-01-06 since the gnome-control-center
    	changes won't happen before 2.18.0. This reopens bug #386411.
    
    svn path=/trunk/; revision=4254

--- gnome-session/gsm-at-startup.h
+++ gnome-session/gsm-at-startup.h
@@ -2,6 +2,7 @@
 #define GSM_AT_STARTUP_H
 
 void gsm_assistive_registry_start (void);
+void gsm_assistive_technologies_start (void);
 void gsm_at_set_gtk_modules (void);
 
 #endif /* GSM_AT_STARTUP_H */
