a8a036ec2d6c0ad5573c7f5f698f7c4448139e46
William Jon McCann <jmccann@redhat.com>
2008-07-21 20:23:18 +0000

    Add some missing includes
    2008-07-21  William Jon McCann  <jmccann@redhat.com>
    
    	* gnome-session/gsm-xsmp-server.c:
    	Add some missing includes
    
    
    
    svn path=/branches/dbus_based/; revision=4821

--- gnome-session/gsm-xsmp-server.c
+++ gnome-session/gsm-xsmp-server.c
@@ -24,8 +24,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
 #include <glib.h>
 #include <glib/gi18n.h>
