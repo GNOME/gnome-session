f4ddeb2fad0c09053e7a5ae40a3784ccd370cb5c
Olav Vitters <ovitters@src.gnome.org>
2008-08-04 14:14:39 +0000

    Like, be gone and stuff
    svn path=/trunk; revision=4902

--- gnome-session/gsm.h
+++
@@ -1,9 +0,0 @@
-#include <glib.h>
-#include "session.h"
-
-#define GSM_GCONF_DEFAULT_SESSION_KEY           "/desktop/gnome/session/default-session"
-#define GSM_GCONF_REQUIRED_COMPONENTS_DIRECTORY "/desktop/gnome/session/required-components"
-
-void gsm_initialization_error (gboolean fatal, const char *format, ...);
-
-extern GsmSession *global_session;
