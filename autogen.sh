b754b094b85b85f437fc55d8a51d694abb5041a2
Vincent Untz <vuntz@gnome.org>
2009-04-24 03:24:50 +0200

    Make autogen.sh more modern.

--- autogen.sh
+++ autogen.sh
@@ -8,16 +8,15 @@ PKG_NAME="GNOME Desktop Session Manager"
 REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.in \
-  && test -f $srcdir/HACKING \
   && test -d $srcdir/gnome-session) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level gnome directory"
+    echo " top-level $PKG_NAME directory"
     exit 1
 }
-
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome-common."
     exit 1
 }
+
 . gnome-autogen.sh
