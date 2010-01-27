b2214df90d82cf5b588858b1a145e26d0f7c8b87
Romain Perier <mrpouet@gentoo.org>
2010-01-27 16:18:34 +0100

    [gsm] Check that the XSMP connection exists when stopping a client
    While this should never happen, it currently happens in some cases. A
    complete fix will come later, but this change doesn't hurt and is
    consistant with what is already done in other functions.
    
    Helps with https://bugzilla.gnome.org/show_bug.cgi?id=598211

--- gnome-session/gsm-xsmp-client.c
+++ gnome-session/gsm-xsmp-client.c
@@ -713,6 +713,14 @@ xsmp_stop (GsmClient *client,
 
         g_debug ("GsmXSMPClient: xsmp_stop ('%s')", xsmp->priv->description);
 
+        if (xsmp->priv->conn == NULL) {
+                g_set_error (error,
+                             GSM_CLIENT_ERROR,
+                             GSM_CLIENT_ERROR_NOT_REGISTERED,
+                             "Client is not registered");
+                return FALSE;
+        }
+
         SmsDie (xsmp->priv->conn);
 
         return TRUE;
