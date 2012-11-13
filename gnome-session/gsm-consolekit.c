e7cbff3fb381c6d1666d148889a5e485d5e2e418
Martin Pitt <martinpitt@gnome.org>
2012-11-13 11:02:35 +0100

    GsmConsoleKit: Properly initialize upower client
    Move UPower client initialization from gsm_consolekit_on_name_owner_changed()
    (which is not actually called for UPower usually) into
    gsm_consolekit_ensure_ck_connection().
    
    This makes the UPower calls actually work, otherwise they fail on trying to
    pass a NULL client.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=688229

--- gnome-session/gsm-consolekit.c
+++ gnome-session/gsm-consolekit.c
@@ -272,6 +272,9 @@ gsm_consolekit_ensure_ck_connection (GsmConsolekit  *manager,
                                              G_CALLBACK (on_active_changed), manager, NULL);
         }
 
+        g_clear_object (&manager->priv->up_client);
+        manager->priv->up_client = up_client_new ();
+
         is_connected = TRUE;
 
  out:
@@ -309,10 +312,8 @@ gsm_consolekit_on_name_owner_changed (DBusGProxy    *bus_proxy,
         }
 
         g_clear_object (&manager->priv->ck_proxy);
-        g_clear_object (&manager->priv->up_client);
 
         gsm_consolekit_ensure_ck_connection (manager, NULL);
-        manager->priv->up_client = up_client_new ();
 
 }
 
