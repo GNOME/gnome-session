GNOME/gnome-session

commit 34c380508516f8a2f81b68d189b24fedb05870ea

main: exit gracefully when disconnected from the session bus
This fixes a problem where all hell breaks loose if we
lose the bus.

https://bugzilla.gnome.org/show_bug.cgi?id=608409

gnome-session/main.c
@@ -54,6 +54,10 @@ static gboolean show_version = FALSE;
 static gboolean debug = FALSE;
 static gboolean please_fail = FALSE;
 
+static DBusGProxy *bus_proxy = NULL;
+
+static void shutdown_cb (gpointer data);
+
 static void
 on_bus_name_lost (DBusGProxy *bus_proxy,
                   const char *name,
@@ -128,7 +132,6 @@ acquire_name_on_proxy (DBusGProxy *bus_proxy,
 static gboolean
 acquire_name (void)
 {
-        DBusGProxy      *bus_proxy;
         GError          *error;
         DBusGConnection *connection;
 
@@ -141,10 +144,22 @@ acquire_name (void)
                 /* not reached */
         }
 
-        bus_proxy = dbus_g_proxy_new_for_name (connection,
-                                               DBUS_SERVICE_DBUS,
-                                               DBUS_PATH_DBUS,
-                                               DBUS_INTERFACE_DBUS);
+        bus_proxy = dbus_g_proxy_new_for_name_owner (connection,
+                                                     DBUS_SERVICE_DBUS,
+                                                     DBUS_PATH_DBUS,
+                                                     DBUS_INTERFACE_DBUS,
+                                                     &error);
+        if (error != NULL) {
+                gsm_util_init_error (TRUE,
+                                     "Could not connect to session bus: %s",
+                                     error->message);
+                /* not reached */
+        }
+
+        g_signal_connect_swapped (bus_proxy,
+                                  "destroy",
+                                  G_CALLBACK (shutdown_cb),
+                                  NULL);
 
         if (! acquire_name_on_proxy (bus_proxy, GSM_DBUS_NAME) ) {
                 gsm_util_init_error (TRUE,
@@ -152,8 +167,6 @@ acquire_name (void)
                                      "Could not acquire name on session bus");
                 /* not reached */
         }
-
-        g_object_unref (bus_proxy);
 
         return TRUE;
 }
@@ -376,6 +389,10 @@ main (int argc, char **argv)
 
         if (client_store != NULL) {
                 g_object_unref (client_store);
+        }
+
+        if (bus_proxy != NULL) {
+                g_object_unref (bus_proxy);
         }
 
         gdm_log_shutdown ();

commit 854edad853880b285309259a79532bef5202514e

main: Fix g_critical() on login
Commit
http://git.gnome.org/browse/gnome-session/commit/?id=34c380508516f8a2f81b68d189b24fedb05870ea
caused us to emit a g_critical because we were passing NULL for the
user data, when the callback expected a GsmManager.  Fixing this
requires passing the user data later.

https://bugzilla.gnome.org/show_bug.cgi?id=688774

gnome-session/main.c
@@ -157,11 +157,6 @@ acquire_name (void)
                 return FALSE;
         }
 
-        g_signal_connect_swapped (bus_proxy,
-                                  "destroy",
-                                  G_CALLBACK (shutdown_cb),
-                                  NULL);
-
         if (! acquire_name_on_proxy (bus_proxy, GSM_DBUS_NAME) ) {
                 gsm_util_init_error (TRUE,
                                      "%s",
@@ -331,6 +326,11 @@ main (int argc, char **argv)
         }
 
         manager = gsm_manager_new (client_store, failsafe);
+
+        g_signal_connect_swapped (bus_proxy,
+                                  "destroy",
+                                  G_CALLBACK (shutdown_cb),
+                                  manager);
 
         g_unix_signal_add (SIGTERM, term_or_int_signal_cb, manager);
         g_unix_signal_add (SIGINT, term_or_int_signal_cb, manager);