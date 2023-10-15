076e10604aedcb7ed0be066f168bfe318b0168cf
Jordan Petridis <jordan@centricular.com>
2023-10-15 17:31:34 +0300

    build: Replace custom post-install hook that compiles schemas
    Meson has a built-in way now to compile the schemas for us.

--- meson_post_install.py
+++ meson_post_install.py
@@ -2,18 +2,12 @@
 
 import os
 import shutil
-import subprocess
 import sys
 
 if os.environ.get('DESTDIR'):
   install_root = os.environ.get('DESTDIR') + os.path.abspath(sys.argv[1])
 else:
   install_root = sys.argv[1]
-
-if not os.environ.get('DESTDIR'):
-  schemadir = os.path.join(install_root, 'glib-2.0', 'schemas')
-  print('Compile gsettings schemas...')
-  subprocess.call(['glib-compile-schemas', schemadir])
 
 # FIXME: Meson is unable to copy a generated target file:
 #        https://groups.google.com/forum/#!topic/mesonbuild/3iIoYPrN4P0
