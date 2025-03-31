2c5534918b385b979449ee1f8f64be7260e1e600
Javier Jardón <jjardon@gnome.org>
2025-03-31 01:12:07 +0100

    gnome-session/gsm-app.c: Do not use deprecated symbols

--- gnome-session/gsm-app.c
+++ gnome-session/gsm-app.c
@@ -37,7 +37,7 @@ typedef struct
         int              phase;
         char            *startup_id;
         gboolean         registered;
-        GTimeVal         last_restart_time;
+        gint64           last_restart_time;
         GDBusConnection *connection;
         GsmExportedApp  *skeleton;
 } GsmAppPrivate;
@@ -513,12 +513,12 @@ gsm_app_restart (GsmApp  *app,
                  GError **error)
 {
         GsmAppPrivate *priv = gsm_app_get_instance_private (app);
-        GTimeVal current_time;
+        gint64 current_time;
         g_debug ("Re-starting app: %s", priv->id);
 
-        g_get_current_time (&current_time);
-        if (priv->last_restart_time.tv_sec > 0
-            && (current_time.tv_sec - priv->last_restart_time.tv_sec) < _GSM_APP_RESPAWN_RATELIMIT_SECONDS) {
+        current_time = g_get_real_time ();
+        if (priv->last_restart_time > 0
+            && (current_time - priv->last_restart_time) < _GSM_APP_RESPAWN_RATELIMIT_SECONDS) {
                 g_warning ("App '%s' respawning too quickly", gsm_app_peek_app_id (app));
                 g_set_error (error,
                              GSM_APP_ERROR,
