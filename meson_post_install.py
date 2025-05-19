GNOME/gnome-session

commit 8a67a74249c1b98ba710c7cc97baefe1c00cbc7a

build: Fix meson_post_install have_x11 assignment
bool('false') is True.

meson_post_install.py
@@ -10,7 +10,7 @@ else:
   install_root = sys.argv[1]
 
 try:
-    have_x11 = bool(sys.argv[2])
+    have_x11 = sys.argv[2] == 'true'
 except IndexError:
     have_x11 = False
 

commit cb25170560dda79660d9499479638ea4f887cb6e

build: Do not run meson_post_install if X11 is disabled
This is not needed at all when X11 is disabled.

Fixes: 8a67a742 ("build: Fix meson_post_install have_x11 assignment")

meson.build
@@ -125,10 +125,12 @@ subdir('data')
 subdir('doc')
 subdir('po')
 
-meson.add_install_script(
-  'meson_post_install.py',
-  [session_datadir, have_x11 ? 'true' : 'false'],
-)
+if have_x11
+  meson.add_install_script(
+    'meson_post_install.py',
+    session_datadir
+  )
+endif
 
 gnome.post_install(
   glib_compile_schemas: true,

meson_post_install.py
@@ -9,12 +9,6 @@ if os.environ.get('DESTDIR'):
 else:
   install_root = sys.argv[1]
 
-try:
-    have_x11 = sys.argv[2] == 'true'
-except IndexError:
-    have_x11 = False
-
-if have_x11:
-    src = os.path.join(install_root, 'xsessions', 'gnome-copy.desktop')
-    dst = os.path.join(install_root, 'xsessions', 'gnome.desktop')
-    shutil.move(src, dst)
+src = os.path.join(install_root, 'xsessions', 'gnome-copy.desktop')
+dst = os.path.join(install_root, 'xsessions', 'gnome.desktop')
+shutil.move(src, dst)