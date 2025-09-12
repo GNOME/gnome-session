GNOME/gnome-session

commit 295e8b853430a1205c26c66f2a05221c523250cb

gnome-session-inhibit: Propagate parent death
When gnome-session-inhibit goes away (via SIGINT or SIGTERM or whatever)
we want the child process to be cleaned up.

Fixes #139

tools/gnome-session-inhibit.c
@@ -25,6 +25,11 @@
 #include <stdint.h>
 #include <locale.h>
 
+#if defined(__linux__)
+#include <linux/prctl.h>
+#include <sys/prctl.h>
+#endif
+
 #include <glib.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
@@ -273,6 +278,12 @@ wait_for_child_app (char **argv)
        * separately from the parent */
       if (isatty (0))
         setpgid (0, 0);
+
+#if defined(__linux__)
+      /* Ensure that the child process goes away when the parent does */
+      if (prctl(PR_SET_PDEATHSIG, SIGHUP) < 0)
+        g_printerr ("Failed to configure PDEATHSIG, ignoring.\n");
+#endif
 
       execvp (*argv, argv);
       g_printerr (_("Failed to execute %s: %m\n"), *argv);

commit 0ef23c3f474cd5a847b6cbc740894e5dc63008dd

gnome-session-inhibit: Fix redundant include of prctl.h
Including libc's prctl.h and linux's prctl.h leads to redundant
declarations of its functions. So just include libc's prctl.h since it
already includes what we need.

Otherwise the following error message can be experienced on musl libc
systems like Alpine Linux:

	In file included from ../tools/gnome-session-inhibit.c:30:
	/usr/include/sys/prctl.h:103:1: error: redefinition of struct or union 'struct prctl_mm_map'
	  103 | };
	      | ^
	In file included from ../tools/gnome-session-inhibit.c:29:
	/usr/include/linux/prctl.h:134:8: note: originally defined here
	  134 | struct prctl_mm_map {
	      |        ^~~~~~~~~~~~

Fixes: 295e8b853430 ("gnome-session-inhibit: Propagate parent death")

tools/gnome-session-inhibit.c
@@ -26,7 +26,6 @@
 #include <locale.h>
 
 #if defined(__linux__)
-#include <linux/prctl.h>
 #include <sys/prctl.h>
 #endif
 